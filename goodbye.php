0b53ed8a05b97a0754905c127182de9b3761eab3
emersonwood <emersonwood94@gmail.com>
2018-02-21 10:48:16 +1300

    Add goodbye & hello

---
+++ goodbye.php
@@ -0,0 +1,15 @@
+<?php
+
+function goodbye() {
+	print "goodbye";
+}
+
+function goodbye2 () {
+        print "goodbye 2";
+}
+
+function goodbye3() {
+	print "goodbye 3";
+}
+
+goodbye();
---
+++ hello.php
@@ -0,0 +1,11 @@
+<?php
+
+function helloworld() {
+	print "hello world 1";
+}
+
+function helloworld2() {
+	print "hello world 2";
+}
+
+helloworld();
